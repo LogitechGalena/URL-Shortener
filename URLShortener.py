4baea243daa5a4d8a76a7cf6d33835efd2d1f65d
LogitechGalena <44323927+LogitechGalena@users.noreply.github.com>
2020-11-17 00:19:53 -0500

    Fixed Issues + Visuals

--- URLShortener.py
+++ URLShortener.py
@@ -1,20 +1,20 @@
-import time
+import requests
 
-def shortenURL()
-    url = input(URL to shorten )
+def shortenURL():
+    url = input("URL to shorten: ")
 
     payload = {
-        url url
+        "url": url
     }
     
-    print(url)
-    r = requests.post('httpscleanuri.comapiv1shorten', data=payload)
-    if error in r.json() 
-        print(Invalid URl. Make sure that URL includes 'https')
-        print(Error )
+    r = requests.post('https://cleanuri.com/api/v1/shorten', data=payload)
+    if "error" in r.json(): 
+        print("Invalid URl. Make sure that URL includes 'https://'")
+        print("Error: ")
         print(r.json())
-    else
-        print(r.json())
+    else:
+        print("Result URL:")
+        print(r.json()["result_url"])
 
-while True
+while True:
     shortenURL()
